325bc6a14a57ecb9b064da78c2a1bc59465db330
Arthur <arthur.pocholle@gmail.com>
2025-01-17 14:38:33 +0100

    Etape 2

--- main.c
+++ main.c
@@ -1,31 +1,41 @@
-    #include <stdio.h>
+#include <stdio.h>
 
-    int main(){
+typedef struct Menu {
+    const char *option;
+} Menu;
 
-        typedef struct Menu
-        {
-            const char *option;
-        }Menu;
+typedef struct Application {
+    const char *nom;
+    Menu *menus;
+    int nb_menus;
+} Application;
 
-        typedef struct Application
-        {
-            const char *nom;
-            Menu *menus;
-            int nb_menus;
+Menu photoMenus[] = {
+    {"Regarder une photo"},
+    {"Prendre une photo"}
+};
 
-        }Application;
+Application photoApp = {
+    .nom = "Photos",
+    .menus = photoMenus,
+    .nb_menus = 2
+};
 
-
-
-        Menu menu1 = {"Option 1"};
-        printf("Menu: %s\n", menu1.option);
-        
-
-
-
-        
-        return 0;
+void displayMenu(const Application* app) {
+    printf("--- %s ---\n", app->nom); 
+    for (int i = 0; i < app->nb_menus; i++) {
+        printf("%d. %s\n", i + 1, app->menus[i].option); 
     }
+    printf("Choisissez une option (0 pour quitter) :\n"); 
+}
+
+int main() {
+    
+    displayMenu(&photoApp);
+
+    return 0;
+}
+
 
     // gcc "./*.c" -o main.exe
     //.\main.exe
